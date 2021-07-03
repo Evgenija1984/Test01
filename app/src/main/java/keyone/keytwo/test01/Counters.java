5a93101074a239d54eedb69f5046281470dec0e2
MEA <mayer_evgenija@mail.ru>
2021-07-03 18:32:35 +0700

    2-й метод Отличается от первого тем, что здесь нет рефлексии; реализовали встроенный в базовый класс View интерфейс OnClickListener, в котором переопределили метод OnClick

--- app/src/main/java/keyone/keytwo/test01/Counters.java
+++ app/src/main/java/keyone/keytwo/test01/Counters.java
@@ -3,16 +3,28 @@ package keyone.keytwo.test01;
 public class Counters {
 
     private int counter_1;
+    private int counter_2;
 
-    public Counters(){
+    public Counters() {
         counter_1 = 0;
+        counter_2 = 0;
+
     }
 
     public int getCounter_1() {
         return counter_1;
     }
 
-    public void increaseCounter1(){
+    public int getCounter_2() {
+        return counter_2;
+    }
+
+    public void increaseCounter1() {
         counter_1++;
     }
+
+    public void increaseCounter2() {
+        counter_2++;
+    }
+
 }
--- app/src/main/java/keyone/keytwo/test01/MainActivity.java
+++ app/src/main/java/keyone/keytwo/test01/MainActivity.java
@@ -12,41 +12,45 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* // метод 1
-    TextView textView1;
-    Button button1;
-    int counter1;
-    // метод 1*/
-
     private TextView textCounter1; // пользовательский элемент 1-го счетчика
+    private TextView textCounter2; // пользовательский элемент 2-го счетчика
+    private TextView textCounter3; // пользовательский элемент 3-го счетчика
+    private TextView textCounter4; // пользовательский элемент 4-го счетчика
     private Button button1;
-    //int counter_1; перенесли в класс Counters
+    private Button button2;
+    private Button button3;
+    private Button button4;
     private Counters counters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    /* // метод 1
-        counter1 = 0;
-        textView1 = findViewById(R.id.textView1);
-        button1 = findViewById(R.id.button1);
-    // метод 1*/
-        //counter_1 = 0; перенесли в класс Counters
         counters = new Counters();
+        initView();
+
+    }
+
+    private void initView() {
         textCounter1 = findViewById(R.id.textView1);
+        textCounter2 = findViewById(R.id.textView2);
+        textCounter3 = findViewById(R.id.textView3);
+        textCounter4 = findViewById(R.id.textView4);
         button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counters.increaseCounter2();
+                textCounter2.setText(String.format(Locale.getDefault(), "%d", counters.getCounter_2()));
+            }
+        });
+
 
     }
 
     public void button1_onClick(View view) {
-    /* // метод 1
-        counter1++;
-        textView1.setText(counter1 + "");
-    // метод 1*/
-        //counter_1++; перенесли в класс Counters
         counters.increaseCounter1();
-        //textCounter1.setText(String.format(Locale.getDefault(), "%d", counter_1));
         textCounter1.setText(String.format(Locale.getDefault(), "%d", counters.getCounter_1()));
 
     }
